6f4acf3ab778debb84190b34202ab13b92cdc425
basilaslam <=>
2025-01-04 01:55:44 +0530

    seo updated

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,10 +5,9 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
